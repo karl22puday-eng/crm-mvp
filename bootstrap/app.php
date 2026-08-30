5aa504bcff747108f6ed9931eca048359f9dfe01
Karl Puday <karl22puday@gmail.com>
2026-08-30 15:58:19 +0800

    Trust proxy for correct HTTPS asset URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserHasRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
